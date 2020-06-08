dbf2db3671db2fa93437d6d480c383ff6adddb5c
D0712936 <56072395+D0712936@users.noreply.github.com>
2020-06-08 22:10:17 +0800

    hi
    hello

---
+++ php_example/action.php
@@ -0,0 +1,19 @@
+<a href = "index.php"> Go Query Interface</a> <p>
+<?php
+	if(isset($_POST['MyHead'])) {
+		$MyHead=$_POST["MyHead"];
+	
+		$dbhost = 'localhost:3307';
+		$dbuser = 'hj';
+		$dbpass = 'test1234';
+		$dbname = 'ods_db';
+		$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
+		mysqli_query($conn, "SET NAMES 'utf8'");
+		mysqli_select_db($conn, $dbname);
+		$sql = "SELECT Pid FROM parkinglot where Ploc LIKE \"%".$MyHead."%\";";
+		$result = mysqli_query($conn, $sql) or die('MySQL query error');
+		while($row = mysqli_fetch_array($result)){
+			echo $row['Pid']."<p>";
+		}
+	}
+?>
